eee1ca30d70950cfa3a5f3b96c1d1782d67179d9
Saul Mendoza - Home <saulmendoza08@gmail.com>
2023-12-09 20:28:33 -0300

    ejercicio terminado

--- cypress/integration/variablesAndFixtures/login.spec.js
+++ cypress/integration/variablesAndFixtures/login.spec.js
@@ -1,13 +1,26 @@
 /// <reference types="Cypress" />
 
 
-context('Formularios',()=>{
+context('Variables',()=>{
     //primero se define la url, en este caso toma la baseUrl+ cy.visit.
     beforeEach(()=>{
         cy.visit('/');
     });
 
-    it('Test #1 : Evitar envio vacio en campos requeridos',()=>{
-        
+    it('Test #1 : Contraseñas incorrectas',()=>{
+
+        cy.fixture('credenciales').as('credenciales');
+        cy.get('@credenciales').then((credenciales)=>{
+            credenciales.usuarios.forEach(usuario=>{
+                credenciales.password.forEach(pass=>{
+                    cy.get('input[name="UserName"]').type(usuario);
+                    cy.get('input[name="Password"]').type(pass);
+                    cy.get('#login').click();
+                    cy.contains('Welcome, '+ usuario + '!')               
+                })
+            })
+        });
+
+       
     });
 });
