c700af207f99c0e4d9fd62975e354534e4ff6a77
Saul Mendoza - Home <saulmendoza08@gmail.com>
2023-12-09 16:58:35 -0300

    primer commit

---
+++ cypress/integration/variablesAndFixtures/login.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+
+context('Formularios',()=>{
+    //primero se define la url, en este caso toma la baseUrl+ cy.visit.
+    beforeEach(()=>{
+        cy.visit('/');
+    });
+
+    it('Test #1 : Evitar envio vacio en campos requeridos',()=>{
+        
+    });
+});
